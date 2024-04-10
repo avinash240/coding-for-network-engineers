2e097fe48137f3c0c8f8968949738d3d4907c00d
Marlon Bailey <mcbailey240@gmail.com>
2024-04-10 23:27:22 +0000

    arguments work

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import json
+import argparse
 lib_path = os.path.join(os.path.dirname(__file__), 'lib')
 sys.path.append(lib_path)
 
@@ -13,10 +15,34 @@ def get_capabilities(channel):
     response = stub.GetCapabilities(request)
     return response
 
+def get_interface_state(channel):
+    request = frr_northbound_pb2.GetRequest()
+    request.path.append("/frr-interface:lib")
+    request.type=frr_northbound_pb2.GetRequest.ALL
+    request.encoding=frr_northbound_pb2.JSON
+    stub = frr_northbound_pb2_grpc.NorthboundStub(channel)
+    return stub.Get(request)
+
 def get_channel(host):
     return grpc.insecure_channel(host)
 
+class SplitArgs(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, [x.strip() for x in values.split(',')])
+
 if __name__ == "__main__":
-    channel = get_channel("172.20.20.3:50051")
-    capabilies = get_capabilities(channel)
-    print(capabilies)
+    parser = argparse.ArgumentParser(
+                    prog='interface-map',
+                    description='generates csv of frr router interfaces',
+                )
+    parser.add_argument('-r', '--router-addresses', action=SplitArgs, required=True, help="comma seperated list of address:grpc-port")
+    parser.add_argument('-o','--output-csv', type=argparse.FileType('w'), required=True, help="output csv filename")
+    args = parser.parse_args()
+    for router in args.router_addresses:
+        #channel = get_channel("172.20.20.3:50051")
+        channel = get_channel(router)
+        capabilies = get_capabilities(channel)
+        print(capabilies)
+        interface_state = get_interface_state(channel)
+        for r in interface_state:
+            print(r.data.data)
